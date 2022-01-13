f36fac18944f67f645fe807784084f8c8ee330df
Fahreza Raharjo <fahrezaraharjo92@gmail.com>
2022-01-13 13:20:24 +0700

    FIKS sum js

--- sum.js
+++ sum.js
@@ -1,12 +1,12 @@
 function sum() {
     let total = 0
 
-    for(var i=0;i<=arguments.length;i++) {
+    for (var i = 0; i < arguments.length; i++) {
         total += arguments[i]
     }
-    console.log (total)
+    console.log(total)
 }
-sum(1,2,7)
-sum(1,4)
+sum(1, 2, 7)
+sum(1, 4)
 sum(11)
-sum(10,3,6,7,9)
+sum(10, 3, 6, 7, 9)
