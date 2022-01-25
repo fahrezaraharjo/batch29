b26765f01daac42781ecba3c574491509c2b5571
Fahreza Raharjo <fahrezaraharjo92@gmail.com>
2022-01-25 17:13:40 +0700

    Tebak Kata Done

--- romawi.js
+++ romawi.js
@@ -28,11 +28,13 @@
 
 function romawi(n) {
 
-  var newword = { M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 }, roman = '', i;
+
+  roman = ''
+  var newword = { M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 },  i;
 
   for(i in newword) {
     while (n >= newword[i]) {
-      roman += i
+      roman += i// roman = masuk key
       n-=newword[i]
     }
   }
