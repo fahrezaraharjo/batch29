8f7299608a6400bf239df0df2d3b191c59307fa1
Fahreza Raharjo <fahrezaraharjo92@gmail.com>
2022-01-26 11:14:22 +0700

    revisi

--- tebaktebakan.mjs
+++ tebaktebakan.mjs
@@ -18,35 +18,32 @@ console.log(soal[index].definition)
 rl.prompt();
 
 rl.on('line', (answer) => {
-    
-    if(answer == "skip") {
+
+    if (answer == "skip") {
         soal.push(soal[index])
         index++
         console.log(soal[index].definition)
-    } else {
-
-
-        
-        if (answer == soal[index].term) {
-            console.log('selamat anda benar!')
-            index++
-            if (index < soal.length) {
-                console.log(soal[index].definition)
-            } else {
-                console.log('Hore Anda Menang!');
-                rl.close()
-            }
-            
+    } else if (answer == soal[index].term) {
+        console.log('selamat anda benar!')
+        index++
+        counter = 0
+        if (index < soal.length) {
+            console.log(soal[index].definition)
         } else {
+            console.log('Hore Anda Menang!');
+            rl.close()
+        }
+
+    } else {
         counter++
         console.log(`"Wkwkwkwk, anda kurang beruntung! anda telah salah ${counter} kali`)
     }
 
-}
+
     rl.prompt();
-    
+
 }).on('close', () => {
     process.exit(0);
 })
-        
-        
+
+
