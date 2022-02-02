6d32ba3f23dc4e3ca7fc59e422390ad76ae54dc4
Fahreza Raharjo <fahrezaraharjo92@gmail.com>
2022-02-02 18:20:52 +0700

    newupdate

--- ch13/todo.js
+++ ch13/todo.js
@@ -1,11 +1,8 @@
-let index = 0
-
 let param = process.argv
 
 const fs = require('fs');
 
 let data = JSON.parse(fs.readFileSync('data3.json', 'utf-8'))
-console.log(data)
 let id = param[3] - 1
 
 
@@ -49,8 +46,14 @@ switch (param[2]) {
 
     case "complete":
 
-        let complete = param[3] - 1
-        data[complete].status == true
+        data[id].complete = true
+
+        fs.writeFileSync('data3.json', JSON.stringify(data, null, 3), 'utf-8')
+        break;
+
+    case "uncomplete":
+
+        data[id].complete = false
 
         fs.writeFileSync('data3.json', JSON.stringify(data, null, 3), 'utf-8')
         break;
@@ -60,7 +63,7 @@ switch (param[2]) {
 
         if (param[3] == 'asc') {
             for (let i = 0; i < data.length; i++)
-                if (!data[i]) console.log(data[i])
+                if (!data[i].complete) console.log(data[i])
             console.log(`${i + 1}. [${data[i].complete ? 'x' : " "}] ${data[i].title}`)
         } else {
             for (let i = data.length - 1; i >= 0; i--)
@@ -72,49 +75,49 @@ switch (param[2]) {
         fs.writeFileSync('data3.json', JSON.stringify(data, null, 3), 'utf-8')
         break;
 
-        case 'completed':
-            if (param[3] == 'asc') {
-                data.forEach((item, index) => {
-                    if (data[index].status == true) {
-                        console.log(`${index + 1}. ${item.status ? '[x]' : '[ ]'} ${item.tugas}`)
-                    }
-                })
-            } else for (let i = data.length - 1; i >= 0; i--) {
-                if (data[i].status == true) {
-                    // console.log(data[i])
-                    console.log(`${i + 1}. ${data[i].status ? '[x]' : '[ ]'} ${data[i].tugas}`)
+    case 'list:completed':
+        console.log('Daftar Pekerjaan')
+        if (param[3] == 'asc') {
+            data.forEach((item, index) => {
+                if (data[index].complete == true) {
+                    console.log(`${index + 1}. ${item.complete ? '[x]' : '[ ]'} ${item.title}`)
                 }
+            })
+        } else for (let i = data.length - 1; i >= 0; i--) {
+            if (data[i].complete == true) {
+                // console.log(todo[i])
+                console.log(`${i + 1}. ${data[i].complete ? '[x]' : '[ ]'} ${data[i].title}`)
             }
-            break;
+        }
+        break;
 
-            case 'tag':
-                let tag = param.slice(3, param.length)
-                console.log(tag)
-        
-                // fs.writeFileSync('todo.json', JSON.stringify(todo), 'utf-8')
-                break;
-        
-    default:
+    case 'tag':
+        let tag = param.slice(4, param.length)
+        //console.log(tag)
+        tag.forEach((item, index) => {
+            data[id].tag.push(tag[index])
+        });
+        //console.log(todo[taskId])
+        //todo[taskId].tag.push(tag)
+        console.log(`'${tag}' telah ditambahkan ke daftar '${data[id].title}'`)
 
-        console.log(">>> JS TODO <<<")
-        console.log("$ node todo.js <command>")
-        console.log("$ node todo.js list")
-        console.log("$ node todo.js task <task_id>")
-        console.log("$ node todo.js add <task_content>")
-        console.log("$ node todo.js delete <task_id>")
-        console.log("$ node todo.js complete <task_id>")
-        console.log("$ node todo.js uncomplete<task_id>")
-        console.log("$ node todo.js list:outstanding asc|dsc")
-        console.log("$ node todo.js list:complited asc|dsc")
-        console.log("$ node todo.js tag <task_id> <tag_name_1> <tag_name_2>...<tag_name_N>")
-        console.log("$ node todo.js filter:<tag_name>")
+        
+        fs.writeFileSync('data3.json', JSON.stringify(data, null, 3), 'utf-8')
 
         break;
+
+    default:
+
+    // console.log(">>> JS data <<<")
+    // console.log("$ node data.js <command>")
+    // console.log("$ node todo.js list")
+    // console.log("$ node todo.js task <task_id>")
+    // console.log("$ node todo.js add <task_content>")
+    // console.log("$ node todo.js delete <task_id>")
+    // console.log("$ node todo.js complete <task_id>")
+    // console.log("$ node todo.js uncomplete<task_id>")
+    // console.log("$ node todo.js list:outstanding asc|dsc")
+    // console.log("$ node todo.js list:complited asc|dsc")
+    // console.log("$ node todo.js tag <task_id> <tag_name_1> <tag_name_2>...<tag_name_N>")
+    // console.log("$ node todo.js filter:<tag_name>")
 }
-
-
-
-// for in object objact { }
-// foreach array[]
-// data[i] dari 1 
-// indek dari 0
