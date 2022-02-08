f774228c94b3358674c774dc6a0efc88c29825a5
Fahreza Raharjo <fahrezaraharjo92@gmail.com>
2022-02-08 21:18:47 +0700

    revisi

--- ch14/query.sql
+++ ch14/query.sql
@@ -1,4 +1,4 @@
--- sqlite3 university.db
+--s sqlite3 university.db
 create table jurusan(
     kodejurusan varchar primary key not null, 
     namajurusan varchar not null
@@ -13,7 +13,7 @@ create table mahasiswa(
     FOREIGN KEY(jurusan) REFERENCES jurusan(kodejurusan)
     );
 
-insert into mahasiswa(nim, name, alamat, jurusan)values("001", "eza", "cijerah", "komputer");
+insert into mahasiswa(nim, name, alamat, jurusan)values("001", "eza", "cijerah", "kodejurusan");
  
 create table dosen(
     kodedosen varchar primary key not null,
@@ -30,15 +30,18 @@ create table matakuliah(
 insert into matakuliah(kodematkul, nama, sks)values('m1', 'ipa', '3');
 
 -- foreign key ada key yang ada di setiap tabel
-
+--key nim yang terdapat pada tabel mahasiswa dengan nama nim
 CREATE TABLE kontrak (
     id integer primary key autoincrement,
     nim varchar not null,
     kodematkul varchar not null,
     kodedosen varchar not null,
     nilai varchar,
-    FOREIGN KEY(nim) REFERENCES mahasiswa(nim)--key nim yang terdapat pada tabel mahasiswa dengan nama nim
+    FOREIGN KEY(nim) REFERENCES mahasiswa(nim)
     FOREIGN KEY(kodematkul) REFERENCES matakuliah(kodematkul)
     FOREIGN KEY(kodedosen) REFERENCES dosen(kodedosen)
     );
+    insert into kontrak(nim, kodematkul, kodedosen)values("001", "m1", "d1");
+
+
 
